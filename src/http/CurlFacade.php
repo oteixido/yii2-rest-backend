8be0284fc7d2d8b8747ff560eaf78833d1e38b4a
oteixido <oriol.teixido@gmail.com>
2018-03-11 20:34:53 +0100

    Rename CurlProxy to CurlFacade

--- src/http/CurlFacade.php
+++ src/http/CurlFacade.php
@@ -3,48 +3,48 @@ namespace oteixido\rest\http;
 
 use yii\base\Component;
 
-class CurlProxy extends Component {
+class CurlFacade extends Component {
     private $_curl = null;
 
     public function open($url)
     {
-        Yii::trace("CurlProxy::init($url)");
+        Yii::trace("CurlFacade::init($url)");
         $this->_curl = curl_init($url);
     }
 
     public function errno()
     {
-        Yii::trace("CurlProxy::errno()");
+        Yii::trace("CurlFacade::errno()");
         return curl_errno($this->_curl);
     }
 
     public function error()
     {
-        Yii::trace("CurlProxy::error()");
+        Yii::trace("CurlFacade::error()");
         return curl_error($this->_curl);
     }
 
     public function getinfo($info)
     {
-        Yii::trace("CurlProxy::getinfo($info)");
+        Yii::trace("CurlFacade::getinfo($info)");
         return curl_getinfo($this->_curl, $info);
     }
 
     public function setopt($option, $value)
     {
-        Yii::trace("CurlProxy::setopt($option)");
+        Yii::trace("CurlFacade::setopt($option)");
         curl_setopt($this->_curl, $option, $value);
     }
 
     public function exec()
     {
-        Yii::trace("CurlProxy::exec()");
+        Yii::trace("CurlFacade::exec()");
         return curl_exec($this->_curl);
     }
 
     public function close()
     {
-        Yii::trace("CurlProxy::close()");
+        Yii::trace("CurlFacade::close()");
         curl_close($this->_curl);
     }
 }
