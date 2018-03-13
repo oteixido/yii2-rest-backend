5472718fb3ececd394be6de485b79abe3afb796e
oteixido <oriol.teixido@gmail.com>
2018-03-13 21:42:59 +0100

    Add HttpClient class

---
+++ src/http/HttpClient.php
@@ -0,0 +1,98 @@
+<?php
+namespace oteixido\rest\http;
+
+use Yii;
+use yii\base\Exception;
+use yii\base\Component;
+
+use oteixido\rest\helpers\UrlHelper;
+
+class HttpClient extends Component {
+    const METHOD_POST   = 'post';
+    const METHOD_GET    = 'get';
+    const METHOD_PUT    = 'put';
+    const METHOD_DELETE = 'delete';
+
+    const HTTP_OK          = 200;
+    const HTTP_CREATED     = 201;
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_NOT_FOUND   = 404;
+
+    public $curl = null;
+    public $baseUrl = '';
+    public $username = '';
+    public $password = '';
+    public $timeout = 10;
+    public $sslVerify = true;
+    public $postParametersAsHttpQuery = true;
+
+    public function __construct($curl, $config = [])
+    {
+        $this->curl = $curl;
+        parent::__construct($config);
+    }
+
+    public function get($url)
+    {
+        return $this->method(self::METHOD_GET, $url);
+    }
+
+    public function post($url, $values)
+    {
+        return $this->method(self::METHOD_POST, $url, $values);
+    }
+
+    public function put($url, $parameters)
+    {
+        return $this->method(self::METHOD_PUT, $url, $parameters);
+    }
+
+    public function delete($url, $parameters)
+    {
+        throw new Exception('Method not implemented');
+    }
+
+    private function method($method, $url, $parameters = [])
+    {
+        Yii::trace("HttpClient::method($method, $url)");
+
+        $this->curl->init(UrlHelper::join([$this->baseUrl, $url]));
+        if (!empty($this->username) || !empty($this->password))
+            $this->curl->setopt(CURLOPT_USERPWD, $this->username.':'.$this->password);
+        $this->curl->setopt(CURLOPT_TIMEOUT, $this->timeout);
+        $this->curl->setopt(CURLOPT_RETURNTRANSFER, true);
+        $this->curl->setopt(CURLOPT_VERBOSE, true);
+        $this->curl->setopt(CURLOPT_HEADER, true);
+        $this->curl->setopt(CURLOPT_SSL_VERIFYHOST, ($this->sslVerify ? 2 : 0));
+        $this->curl->setopt(CURLOPT_SSL_VERIFYPEER, ($this->sslVerify ? 1 : 0));
+        if ($method == self::METHOD_POST) {
+            $this->curl->setopt($curl, CURLOPT_POST, true);
+            $this->curl->setopt($curl, CURLOPT_POSTFIELDS, $this->postParametersAsHttpQuery ? http_build_query($parameters) : $parameters);
+        }
+        if ($method == self::METHOD_PUT) {
+            $this->curl->setopt(CURLOPT_CUSTOMREQUEST, 'PUT');
+            $this->curl->setopt(CURLOPT_POSTFIELDS, http_build_query($parameters));
+        }
+        $result = $this->curl->exec();
+        $curl_errno = $this->curl->errno();
+        $curl_error = $this->curl->error();
+        $curl_httpCode = $this->curl->getinfo(CURLINFO_HTTP_CODE);
+
+        $header_size = $this->curl->getinfo(CURLINFO_HEADER_SIZE);
+        $header_text = substr($result, 0, $header_size);
+        $body = substr($result, $header_size);
+
+        $headers = [];
+        foreach(explode("\n", $header_text) as $header) {
+            $middle = explode(":",$header);
+            if (count($middle) == 2)
+                $headers[trim($middle[0])] = trim($middle[1]);
+        }
+        $this->curl->close();
+        if ($curl_errno > 0) {
+            throw new Exception($curl_error, $curl_errno);
+        }
+        Yii::trace("HttpClient::method($method, $url) => httpCode=$curl_httpCode");
+        return new HttpResponse($code = $curl_httpCode, $body, $headers);
+    }
+}
---
+++ tests/unit/http/HttpClientTest.php
@@ -0,0 +1,56 @@
+<?php
+use \Codeception\Stub\Expected;
+
+use oteixido\rest\http\HttpClient;
+use oteixido\rest\http\CurlFacade;
+
+class HttpClientTest extends \Codeception\Test\Unit
+{
+    public $client;
+    public $curl;
+
+    protected function _before()
+    {
+        $headers = [
+            'header-1: value-header-1',
+        ];
+        $content = 'Body Content';
+        $this->curl =  $this->makeEmpty(CurlFacade::className(), [
+            'exec' => function () use ($headers, $content) {
+                return implode("\n", $headers)."\n".$content;
+            },
+            'getinfo' => function ($option) use ($headers, $content) {
+                if ($option == CURLINFO_HTTP_CODE) return 200;
+                if ($option == CURLINFO_HEADER_SIZE) return strlen(implode("\n", $headers)."\n");
+            },
+        ]);
+        $this->client = new HttpClient($this->curl);
+    }
+
+    public function testGet_ok()
+    {
+        \Codeception\Stub::update($this->curl, [
+            'errno' => function () {
+                return 0;
+            },
+            'error' => function() {
+                return '';
+            },
+        ]);
+        $httpResponse = $this->client->get('url');
+        $this->tester->assertEquals(200, $httpResponse->getCode());
+        $this->tester->assertEquals('Body Content', $httpResponse->getContent());
+        $this->tester->assertEquals('value-header-1', $httpResponse->getHeader('header-1'));
+    }
+
+    public function testGet_error()
+    {
+        \Codeception\Stub::update($this->curl, [
+            'errno' => function () { return 500; },
+            'error' => function() { return 'Error 500'; }
+        ]);
+        $this->tester->expectException(new Exception('Error 500', 500), function() {
+            $this->client->get('url');
+        });
+    }
+}
